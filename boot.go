e765cdfd482bd8e09e9ce99d2518df40fe242d16
Aaron Schlesinger <aschlesinger@deis.com>
2016-01-26 16:06:44 -0800

    fix(boot.go): run the planning server & startup script in goroutines
    so that one doesn’t block the other from progressing

--- boot.go
+++ boot.go
@@ -13,25 +13,41 @@ import (
 )
 
 func main() {
-	bootCmd := exec.Command("/bin/start_riak")
-	bootCmd.Stdout = os.Stdout
-	bootCmd.Stderr = os.Stderr
-	if err := bootCmd.Run(); err != nil {
-		log.Printf("Error running riak start script (%s)", err)
-		os.Exit(1)
+	cmdDoneCh := make(chan error)
+	go func() {
+		bootCmd := exec.Command("/bin/start_riak")
+		bootCmd.Stdout = os.Stdout
+		bootCmd.Stderr = os.Stderr
+		if err := bootCmd.Run(); err != nil {
+			cmdDoneCh <- err
+		}
+	}()
+
+	serverDoneCh := make(chan error)
+	if os.Getenv("RIAK_MASTER") == "1" {
+		go func() {
+			var mut sync.Mutex
+			httpPort, err := strconv.Atoi(os.Getenv("PLANSRV_HTTP_PORT"))
+			if err != nil {
+				httpPort = plansrv.DefaultHTTPPort
+			}
+			hostStr := fmt.Sprintf(":%d", httpPort)
+			log.Printf("Serving cluster planner on %s", hostStr)
+			mux := http.NewServeMux()
+			mux.Handle("/plan_and_commit", plansrv.NewPlanAndCommitHandler(&mut))
+
+			if err := http.ListenAndServe(hostStr, mux); err != nil {
+				serverDoneCh <- err
+			}
+		}()
 	}
 
-	if os.Getenv("RIAK_MASTER") == "1" {
-		var mut sync.Mutex
-		httpPort, err := strconv.Atoi(os.Getenv("PLANSRV_HTTP_PORT"))
-		if err != nil {
-			httpPort = plansrv.DefaultHTTPPort
-		}
-		hostStr := fmt.Sprintf(":%d", httpPort)
-		log.Printf("Serving cluster planner on %s", hostStr)
-		mux := http.NewServeMux()
-		mux.Handle("/plan_and_commit", plansrv.NewPlanAndCommitHandler(&mut))
-
-		go http.ListenAndServe(hostStr, mux)
+	select {
+	case err := <-cmdDoneCh:
+		log.Printf("Error running riak start script (%s)", err)
+		os.Exit(1)
+	case err := <-serverDoneCh:
+		log.Printf("Error running plan/commit server (%s)", err)
+		os.Exit(1)
 	}
 }
