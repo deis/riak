e9a3c3f66dda305fd46fac4e22790bda9282f0c7
Aaron Schlesinger <aschlesinger@deis.com>
2016-01-27 13:37:34 -0800

    doc(server.go): add godoc

--- clustersrv/server.go
+++ clustersrv/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Start starts the cluster server on the given port. The server will block execution of this function as long as it runs. If it stops, Start sends an error on doneCh
 func Start(port int, doneCh chan<- error) {
 	var mut sync.Mutex
 	lockID := NewLockID()
