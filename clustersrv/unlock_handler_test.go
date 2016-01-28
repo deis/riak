f5aac309d7411eae020e8c4d7e834ef5e931bcae
Aaron Schlesinger <aschlesinger@deis.com>
2016-01-28 11:28:01 -0800

    fix(unlock_handler_test.go): write tests for unlock handler

---
+++ clustersrv/unlock_handler_test.go
@@ -0,0 +1,46 @@
+package clustersrv
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+
+	"github.com/arschles/assert"
+	"github.com/gorilla/mux"
+)
+
+func TestUnlockNotLocked(t *testing.T) {
+	mut := new(sync.Mutex)
+	mut.Lock()
+	lockID := NewLockID()
+	router := mux.NewRouter()
+	registerUnlockHandler(router, mut, lockID)
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("DELETE", "/lock", bytes.NewReader(nil))
+	assert.NoErr(t, err)
+	router.ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusBadRequest, "response code")
+
+	w := httptest.NewRecorder()
+	r, err = http.NewRequest("DELETE", "/lock/noexist", bytes.NewReader(nil))
+	assert.NoErr(t, err)
+	router.ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusBadRequest, "response code")
+}
+
+func TestUnlockAlreadyLocked(t *testing.T) {
+	mut := new(sync.Mutex)
+	lockID := NewLockID()
+	lockID.Generate()
+	router := mux.NewRouter()
+	registerUnlockHandler(router, mut, lockID)
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("DELETE", "/lock/"+lockID.id, bytes.NewReader(nil))
+	assert.NoErr(t, err)
+	router.ServeHTTP(w, r)
+	assert.Equal(t, w.Code, http.StatusOK, "response code")
+}
