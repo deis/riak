d07201ba4b0c7c7ea7302649036ea14a695c2b4d
Aaron Schlesinger <aschlesinger@deis.com>
2016-01-28 16:23:58 -0800

    fix(cs): move all install logic into Dockerfile

--- cs/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HOSTNAME=`hostname -f`
-FILENAME=/etc/apt/sources.list.d/basho.list
-OS=ubuntu
-DIST=trusty
-PACKAGE_CLOUD_RIAK_CS_DIR=https://packagecloud.io/install/repositories/basho/riak-cs
-curl -s "${PACKAGE_CLOUD_RIAK_CS_DIR}/config_file.list?os=${OS}&dist=${DIST}&name=${HOSTNAME}" > $FILENAME
