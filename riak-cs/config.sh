635f924e1f61333135384c3e77d6b32a61d95ab0
Aaron Schlesinger <aschlesinger@deis.com>
2016-01-28 17:12:41 -0800

    fix(riak-cs/config.sh): add LISTEN_HOST and LISTEN_PORT config

--- riak-cs/config.sh
+++ riak-cs/config.sh
@@ -4,9 +4,23 @@ CONF_FILE=/etc/riak-cs/riak-cs.conf
 IP_ADDRESS=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 STANCHION_URL=${DEIS_RIAK_STANCHION_SERVICE_HOST}:${DEIS_RIAK_STANCHION_SERVICE_PORT}
 
+if [ "$LISTEN_HOST" == "" ]; then
+  echo "Error: LISTEN_HOST not set"
+  exit 1
+fi
+if [ "$LISTEN_PORT" == "" ]; then
+  echo "Error LISTEN_PORT not set"
+  exit 1
+fi
+if [ "$STANCHION_URL" == "" ]; then
+  echo "STANCHION_URL not set, exiting"
+  exit 1
+fi
+
 ADMIN_KEY=$(cat /var/run/secrets/deis/riakcs/admin-user)
 ADMIN_SECRET=$(cat /var/run/secrets/deis/riakcs/admin-secret)
 
+sed -i.bak "s/listener = 127.0.0.1:8080/listener = $LISTEN_HOST:$LISTEN_PORT" $CONF_FILE
 sed -i.bak \
   "s/stanchion_host = stanchion_host = 127.0.0.1:8085/stanchion_host = $STANCHION_URL" \
    $CONF_FILE
