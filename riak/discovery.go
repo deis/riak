c16b095945379386ea16a3e14eda26974d520b2e
Matthew Fisher <mfisher@engineyard.com>
2016-02-03 16:05:55 -0800

    fix(discovery.go): check against local IP and loopback addresses
    Gotta make sure we're not trying to join ourselves!

--- riak/discovery.go
+++ riak/discovery.go
@@ -17,8 +17,28 @@ func getDiscoveryIP(hostName string) (string, error) {
 	discoveryAddrs, err := net.LookupHost(hostName)
 	if err != nil {
 		return "", err
-	} else if len(discoveryAddrs) == 0 {
-		return "", ErrNoDiscoveryAddrs
 	}
-	return discoveryAddrs[0], nil
+	for _, addr := range discoveryAddrs {
+		if addr != localIP() && !net.ParseIP(addr).IsLoopback() {
+			return addr, nil
+		}
+	}
+	return "", ErrNoDiscoveryAddrs
+}
+
+// localIP returns the non loopback local IP of the host
+func localIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback then display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
